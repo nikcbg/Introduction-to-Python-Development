0465c5f023ab253268419ff0a4d8fce179cb2027
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-08-30 10:07:16 +0300

    Create creating_classes.py

---
+++ Object-Oriented Programming Basics/Creating Classes/creating_classes.py
@@ -0,0 +1,11 @@
+class Car:
+    """
+    Car models a car w/ tires and an engine
+    """
+
+    def __init__(self, engine, tires):
+        self.engine = engine
+        self.tires = tires
+
+    def description(self):
+        print(f"A car with an {self.engine} engine, and {self.tires} tires")
