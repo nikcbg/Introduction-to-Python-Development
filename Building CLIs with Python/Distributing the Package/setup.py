3441288664ee60d3268938e952b9766f03b80f03
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-09-02 13:50:28 +0300

    Create setup.py

---
+++ Building CLIs with Python/Distributing the Package/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Keith Thompson',
+    author_email='your@email.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/your-uername/pgbackup',
+    packages=find_packages('src')
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ],
+    }
+)
+
